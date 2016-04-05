c92659eca1e06f15afc28acd0df594ffbdb79894
Luke Amdor <luke.amdor@gmail.com>
2016-04-04 21:22:41 -0500

    fix to use https for local since big 2.0

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-TF_LOG=TRACE TF_LOG_PATH=./test-sh-tf.log TF_ACC=yes MARATHON_URL=${MARATHON_URL:="http://marathon.dev.banno.com"} go test ./marathon -v
+TF_LOG=TRACE TF_LOG_PATH=./test-sh-tf.log TF_ACC=yes MARATHON_URL=${MARATHON_URL:="https://marathon.dev.banno.com"} go test ./marathon -v
