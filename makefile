c42c7a2a1d9ea6b5f33602a85602d56ebf948126
DustinChaloupka <dustin.chaloupka@banno.com>
2016-11-30 14:41:11 -0600

    Use local docker-compose yaml file

--- makefile
+++ makefile
@@ -9,14 +9,24 @@ linux:
 osx:
 	GOOS=darwin GOARCH=386 go build -o bin/terraform-provider-marathon-osx .
 
-build: vet osx linux
+install:
 	go install .
 
-test: build
-	big inventory add marathon
-	big up -d marathon
-	sleep 5
-	TF_LOG=TRACE TF_LOG_PATH=./test-sh-tf.log TF_ACC=yes MARATHON_URL=https://marathon.dev.banno.com go test ./marathon -v
+os=$(shell uname)
+ifeq ($(os),Darwin)
+  DEV_BANNO_IP=192.168.99.100
+  LOCAL_BANNO_IP=192.168.99.1
+else
+  DEV_BANNO_IP=127.0.0.1
+  LOCAL_BANNO_IP=127.0.0.1
+endif
 
-release:
+test: install
+	DEV_BANNO_IP=$(DEV_BANNO_IP) LOCAL_BANNO_IP=$(LOCAL_BANNO_IP) docker-compose up -d
+	sleep 5
+	TF_LOG=TRACE TF_LOG_PATH=./test-sh-tf.log TF_ACC=yes MARATHON_URL=http://dev.banno.com:8080 go test ./marathon -v
+	docker-compose kill
+	docker-compose rm -f
+
+release: vet linux osx
 	./bin/release.sh
--- marathon/provider_test.go
+++ marathon/provider_test.go
@@ -1,22 +1,11 @@
 package marathon
 
 import (
-	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
 )
-
-var testAccProviders map[string]terraform.ResourceProvider
-var testAccProvider *schema.Provider
-
-func init() {
-	testAccProvider = Provider().(*schema.Provider)
-	testAccProviders = map[string]terraform.ResourceProvider{
-		"marathon": testAccProvider,
-	}
-}
 
 func TestProvider(t *testing.T) {
 	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
@@ -26,10 +15,4 @@ func TestProvider(t *testing.T) {
 
 func TestProvider_impl(t *testing.T) {
 	var _ terraform.ResourceProvider = Provider()
-}
-
-func testAccPreCheck(t *testing.T) {
-	if v := os.Getenv("MARATHON_URL"); v == "" {
-		t.Fatal("MARATHON_URL must be set for the acceptance tests to work.")
-	}
 }
